62704153b5f791eb03b670f291aa830aea057174
Adam K <a.kwiatkowski.21@unimail.winchester.ac.uk>
2024-04-17 23:08:42 +0100

    Added favicon and fixed basic CSS

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import configparser
 
 from flask import (
     Flask,
-    g, # Global data context for this request
+    g,  # Global data context for this request
     redirect,
     render_template,
     request,
     send_from_directory,
     url_for,
-    current_app
+    current_app,
 )
 from flask_pymongo import PyMongo
 from werkzeug.local import LocalProxy
@@ -19,8 +19,8 @@ config.read(os.path.abspath(os.path.join(".ini")))
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
-app.config['MONGO_URI'] = config['TEST']['DB_URI']
+app.config["DEBUG"] = True
+app.config["MONGO_URI"] = config["TEST"]["DB_URI"]
 
 mongo = PyMongo(app)
 
@@ -29,7 +29,7 @@ capp = current_app
 
 with app.app_context():
     db = g._database = mongo.db
-    #db = LocalProxy(db)
+    # db = LocalProxy(db)
 
 if db is None:
     raise Exception("Unable to connect to database.")
@@ -37,12 +37,17 @@ if db is None:
 print("Collections:")
 print(db.list_collection_names())
 
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory( os.path.join(app.root_path, "static", "media"), "icon.ico" ) #TODO: Ensure that favicon is appropriate size
+
+
 @app.route("/")
 def index():
-
     testcollection = db.get_collection("testcollection")
-    testitem = testcollection.find_one({"profession":"walker"})
+    testitem = testcollection.find_one({"profession": "walker"})
     print(testitem)
 
     print("Request for index page received")
-    return render_template("index.html", testitem = testitem)
+    return render_template("index.html", testitem=testitem)
